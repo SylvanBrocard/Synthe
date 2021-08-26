8c6ecbd4cebb5c842684067f7379e6682effcebb
SylvanBrocard <sylvan.brocard@gmail.com>
2021-08-26 09:21:59 +0200

    first try for FT

---
+++ src/FT.cpp
@@ -0,0 +1,16 @@
+#include <complex>
+#include <math.h>
+
+void FT(float *buffer, int buffer_size, float* bands, int n_bands)
+{
+    std::complex<float> integral;
+    const std::complex<float> I(0.0,1.0);
+
+    for(int i = 0; i < n_bands; i++){
+        integral = {0.0, 0.0};
+        for(int j = 0; j < buffer_size; j++){
+            integral += buffer[j] * std::exp(I * (float)(j * i));
+        }
+        bands[i] = integral.real() / sqrt(2 * M_PIf32);
+    }
+}
---
+++ src/FT.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void FT(float *, int , float* , int);
